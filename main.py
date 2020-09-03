a5e7bd055f2a678a91dee6e6ea3d8771f8ffddb4
Mark Wolff <wolffm0@hartwick.edu>
2020-09-03 12:53:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Start typing your program below this line..
+print("Hello World!")
+print("What is my name?")
+myName = input()
+print('It is good to meet you, ' + myName)
+
+# when working with functions, you provide input.
+# The input is called _arguments_.
+# invoke
+# pass an _arguments_
+# return
+# 
